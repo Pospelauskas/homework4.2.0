e87aa99d0cfe9b84c1cee1fd7fdcf0300f646d14
Pospelauskas <pospelovlesa426@gmail.com>
2024-07-14 22:49:03 +0300

    Add files via upload

---
+++ homework4.2.0.py
@@ -0,0 +1,8 @@
+my_string = input("Привет Леша ")
+print(len("Привет леша"))
+print("Привет Леша". upper())
+print("Привет Леша".lower())
+print("Привет Леша".replace(" ",""))
+print(input("Привет Леша"[0]))
+print(input("Привет Леша"[-1]))
+
